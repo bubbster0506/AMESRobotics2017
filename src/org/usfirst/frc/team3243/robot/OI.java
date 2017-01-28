8acd06d468ab4f22dcd474bad8e3618025d21e72
Mark <asdf@asdf.com>
2017-01-27 18:51:29 -0700

    Fixed OI imports

--- src/org/usfirst/frc/team3243/robot/OI.java
+++ src/org/usfirst/frc/team3243/robot/OI.java
@@ -1,12 +1,8 @@
 package org.usfirst.frc.team3243.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.Trigger;
 
 /**
  * This class is the glue that binds the controls on the physical operator
