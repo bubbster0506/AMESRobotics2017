a0f5e0f0ace2a463f251321cf1fa4b5c95905a97
Riley <orileysorenesn@gmail.com>
2017-01-27 18:25:41 -0700

    Fixed the nothing important

---
+++ src/org/usfirst/frc/team3243/robot/subsystems/Arm.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team3243.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Arm extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/org/usfirst/frc/team3243/robot/subsystems/Motors.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team3243.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Motors extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
